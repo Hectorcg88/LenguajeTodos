11207b36a147ced40f1cfb5ae600784e3fd5d096
Hectorcg88 <casesgimenezh@gmail.com>
2023-04-24 17:15:02 +0200

    Version 0

---
+++ js/navegation.js
@@ -0,0 +1,44 @@
+/**
+ * Con esta funcion aparece un mensaje en pantalla al darle al boton deseado para indicar que todo ha fincionado correctamente
+ */
+
+function hola(){
+  swal("Gracias jefe!", "Te responderé en cuanto pueda", "success");
+}
+
+/**
+ * Esta funcion indica al cliente que el objeto se ha añadido correctamente a la cesta
+ */
+
+function agregarCesta(){
+  swal("Producto agregado", "Accede a la cesta para terminar la compra", "success");
+}
+
+/**
+ * Declaración de constantes para hacer los menus
+ */
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+/**
+ * Funcion para desplegar el menu
+ */
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+/**
+ * Funcion para cerrar el menu
+ */
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+/**
+ * Esta funcion muestra un mensaje en pantalla al darle a un boton
+ */
+
+function mostrarMensaje() {
+  alert("Enhorabuena, has encontrado un descuento especial, con el codigo QWER-HFAS-IAFJ tendras un 10% de descuento en tu proxima compra");
+}
